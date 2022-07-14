328350dff35ccad850db9be0e549a55c1897927e
Corrosive <colton.bruni@gmail.com>
2022-07-13 22:15:31 -0500

    clean slate

---
+++ The Paper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Paper
+//
+//  Created by Colton Bruni on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
